0b21c8048fb642db4cfb68c37ff66b4272e991b7
theshifaligwal <gwal.shifali2223@gmail.com>
2021-08-08 04:58:30 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Container, Row, Col } from "reactstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+
+import Axios from "axios";
 
 function App() {
   return (
